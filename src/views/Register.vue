c14dade52f8893d582a6552ec2fc18a4d6d9cee9
identityphobe <identityphobe@tutanota.com>
2021-10-09 16:04:16 +0800

    Add basic registration view

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -13,10 +13,27 @@
       <div class="field">
         <label class="label">Password</label>
         <div class="control">
-          <input class="input is-success" type="text" placeholder="Password" />
+          <input
+            class="input is-success"
+            type="password"
+            placeholder="Password"
+          />
         </div>
         <p class="help is-success">This username is available</p>
         <p class="help is-danger">This username is unavailable</p>
+      </div>
+
+      <div class="field">
+        <label class="label">Subject</label>
+        <div class="control">
+          <div class="select">
+            <select>
+              <option>Computing</option>
+              <option>Informatics</option>
+              <option>Foundation and Diploma Studies</option>
+            </select>
+          </div>
+        </div>
       </div>
 
       <div class="field is-grouped is-grouped-centered">
@@ -131,6 +148,6 @@
 
 <script>
 export default {
-  name: "Login",
+  name: "Register",
 };
 </script>
